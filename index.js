a101c1b30ec52e82669e2cff5ce65709b0433995
Fabio Chebabe Carnelli <fabio.carnelli@atomicbuild.com>
2023-06-12 22:34:35 -0300

    set up the routes for application

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const mongoose = require('mongoose');
 
 const app = express();
 const port = 3003;
+const routes = require('./routes');
 
 app.use(express.json());
-
-app.get('/', (request, response) => {
-  response.status(200).json({ success: 'Sucesso ao conectar a rota' });
-});
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
